15ac61fd99871c8a437ee3599b0646c8f0d08465
santoshcodes1-max <santosh.codes1@gmail.com>
2026-06-26 17:50:32 +0530

    Time: 79 ms (54.3%), Space: 114.9 MB (48.34%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
-        //Instead of map you can use an array of size 2*n+1 as well because we can have cumulaive sum from -n to +n)
-        unordered_map<int,int> mp;   // mp[cumSum] = frequency of how many times cumSum has occurred
+
+        unordered_map<int,int> mp;   
         
         int currSum = 0;
         
-        mp[0] = 1; //we have seen cumSum = 0 in the beginning once
+        mp[0] = 1; 
 
         long long validLeftPoints = 0;
 
